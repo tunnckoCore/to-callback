df5f31e95cf25992f72b27775127bff44ece937b
tunnckoCore <mameto_100@mail.bg>
2016-11-03 20:02:48 +0200

    chore(init): implement

---
+++ index.js
@@ -0,0 +1,34 @@
+/*!
+ * to-callback <https://github.com/tunnckoCore/to-callback>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var tryCatch = require('try-catch-callback')
+
+module.exports = function resultFirst2callback (fn) {
+  return function () {
+    var args = [].slice.call(arguments)
+    var errFirstCb = args[args.length - 1]
+
+    if (typeof errFirstCb !== 'function') {
+      throw new TypeError('to-callback: expect last argument to be function')
+    }
+    if (errFirstCb.length < 1) {
+      throw new Error('to-callback: expect error-first callback')
+    }
+
+    function resFirstCb (res) {
+      errFirstCb(null, res)
+    }
+
+    tryCatch(fn, {
+      args: args.slice(0, -1).concat(resFirstCb)
+    }, function (err) {
+      if (err) errFirstCb(err)
+    })
+  }
+}
---
+++ test.js
@@ -0,0 +1,55 @@
+/*!
+ * to-callback <https://github.com/tunnckoCore/to-callback>
+ *
+ * Copyright (c) 2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+var toCallback = require('./index')
+var EventEmitter3 = require('eventemitter3')
+var emitter = new EventEmitter3()
+
+test('should throw TypeError if last argument not a function', function (done) {
+  function fixture () {
+    toCallback(emitter.on)(123)
+  }
+  test.throws(fixture, TypeError)
+  test.throws(fixture, /expect last argument to be function/)
+  done()
+})
+
+test('should throw if last argument not a function', function (done) {
+  function fixture () {
+    toCallback(emitter.on)(123, function () {})
+  }
+  test.throws(fixture, Error)
+  test.throws(fixture, /expect error-first callback/)
+  done()
+})
+
+test('should convert result-first to error-first callback', function (done) {
+  var onCallback = toCallback(emitter.on.bind(emitter))
+  onCallback('foo', function errFirstCb (err, res) {
+    test.strictEqual(err, null)
+    test.strictEqual(res, 123)
+    done()
+  })
+  emitter.emit('foo', 123)
+})
+
+test('should get err if result-first API throws', function (done) {
+  function resfirst (val, resultFirstCb) {
+    if (val !== 22) throw new Error('some err')
+    resultFirstCb(val)
+  }
+  var errfirst = toCallback(resfirst)
+  errfirst(11, function (err) {
+    test.strictEqual(err && err.message, 'some err')
+    done()
+  })
+})
